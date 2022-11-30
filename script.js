67d5060d302c1fe9a825e9790e46678ec3950ef2
serp0v <serpov.cc@mail.ru>
2022-11-30 22:24:27 +1000

    add pages and btns id's & onclick func

--- script.js
+++ script.js
@@ -1,11 +1,62 @@
-const robots = document.querySelector('#robots');
-const homePage = document.querySelector('#homePage');
-const robotsPage = document.querySelector('#robotsPage');
+// query header btns
+const logoBtn = document.querySelector('#logoBtn');
+const homeBtn = document.querySelector('#homeBtn');
+const robotsBtn = document.querySelector('#robotsBtn');
+const softwareBtn = document.querySelector('#softwareBtn');
+const contactBtn = document.querySelector('#contactBtn');
 const logBtn = document.querySelector('#logBtn');
 const signBtn = document.querySelector('#signBtn');
-robots.onclick = () => {
+
+// query header pages
+
+const homePage = document.querySelector('#homePage');
+const robotsPage = document.querySelector('#robotsPage');
+const softwarePage = document.querySelector('#softwarePage');
+const contactPage = document.querySelector('#contactPage');
+
+// query header forms
+
+const logForm = document.querySelector('#logForm');
+const signForm = document.querySelector('#signForm');
+
+// header nav onclick (dn = css display none class)
+
+function displayNonePages() {
+    robotsPage.classList.add('dn');
     homePage.classList.add('dn');
-    robotsPage.classList.remove('dn');
+    softwarePage.classList.add('dn');
+    contactPage.classList.add('dn');
+} 
+
+logoBtn.onclick = () => {
+    if (homePage.classList = 'dn') {
+        displayNonePages();
+        homePage.classList.remove('dn');
+    }
+}
+homeBtn.onclick = () => {
+    if (homePage.classList = 'dn') {
+        displayNonePages();
+        homePage.classList.remove('dn');
+    }
+}
+robotsBtn.onclick = () => {
+    if (robotsPage.classList = 'dn') {
+        displayNonePages();
+        robotsPage.classList.remove('dn');
+    }
+}
+softwareBtn.onclick = () => {
+    if (softwarePage.classList = 'dn') {
+        displayNonePages();
+        softwarePage.classList.remove('dn');
+    }
+}
+contactBtn.onclick = () => {
+    if (contactPage.classList = 'dn') {
+        displayNonePages();
+        contactPage.classList.remove('dn');
+    }
 }
 logBtn.onclick = () => {
     logForm.classList.remove('dn');
@@ -13,3 +64,6 @@ logBtn.onclick = () => {
 signBtn.onclick = () => {
     signForm.classList.remove('dn');
 }
+
+
+// move id's to <1200px width design
